14af4bdf4d4d722c3080a05d7077e775a88c5578
KevinGuzmanH <82849364+KevinGuzmanH@users.noreply.github.com>
2021-05-09 20:58:54 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MiComponentComponent } from './components/mi-componente/mi-componente.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MiComponentComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
